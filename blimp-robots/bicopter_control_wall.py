65e7113546d2b4539cd289023472687c832efede
David <dajulian@gmail.com>
2023-09-30 23:36:48 -0400

    wall distance control

--- blimp-robots/bicopter_control_wall.py
+++ blimp-robots/bicopter_control_wall.py
@@ -51,54 +51,40 @@ try:
                 # fx = 1.4
 
                 # Compute points in the body frame
-                x1, y1 = dist_1 * cos(pi / 6), dist_1 * sin(pi / 6)
-                x2, y2 = dist_2 * cos(-pi / 6), dist_2 * sin(-pi / 6)
+                beta = pi / 6  # Angle between the y-axis and the ray of the distance sensor.
+                x1, y1 = dist_1 * cos(pi / 2 - beta), dist_1 * sin(pi / 2 - beta)  # right point
+                x2, y2 = dist_2 * cos(pi / 2 + beta), dist_2 * sin(pi / 2 + beta)  # left point
 
-                # Line y=ax + b
+                # Line y = ax + b
                 a = (y1 - y2) / (x1 - x2)
-                b = y2 - a * x2
-                b = (dist_1 + dist_2) / 2
+                b = y2 - a * x2     # Distance to the front of the robot (y-axis)
+                # b = (dist_1 + dist_2) / 2
                 # relative angle
                 ang = atan2(y1 - y2, x1 - x2)
                 # distance
 
                 # desired distance
-                dd = 4
+                dd = 3.5
 
-                fx = -.1 * (dd - b)
-                # fx = 0
-                torque = -.01 * (pi / 2 - ang)
+                kp_rot = .01
+                kp_forward = -.1
+                fx = kp_forward * (dd - b)
+                torque = kp_rot * (0 - ang)
                 print(degrees(ang), b, fx)
-
-
-
-
-
-
 
             else:
                 torque = rotate
         else:
-            if det_2 == True:
+            if det_2:
                 fx = .1
                 torque = -rotate
             else:
                 fx = 1.4
 
 
-        # print(front_det,rear_det, fx, sp)
+        theta = atan2(fz, fx)  # Servo angle
+        f = hypot(fz, fx)  # Motor force
 
-
-        # force to move forward
-        # fx = 0 * .1
-
-        theta = atan2(fz, fx)
-        f = hypot(fz, fx)
-
-
-        # print(math.degrees((theta)), rz, vx)
-
-        base_angle = radians(90)
 
         # Control servos and motors
         r.set_servo_forces(servo_angle1=theta - torque, servo_angle2=theta + torque, force_motor1=f, force_motor2=f)
